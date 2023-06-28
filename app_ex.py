41b40f5c9da1b20d7293b96db215753f66bbf2b8
bi-cnc <137881662+bi-cnc@users.noreply.github.com>
2023-06-28 14:38:12 +0200

    Update app_ex.py

--- app_ex.py
+++ app_ex.py
@@ -110,7 +110,6 @@ if st.button('Spočítat'):
         converted_amount = converter.convert(amount, base_currency, target_currency)
 
         # Zobrazení výsledku
-        st.markdown(f'<div style="border: 1px solid red; padding: 10px; color: red; border-radius: 5px; display: inline-block;">{amount} {base_currency} = {converted_amount} {target_currency}</div>', unsafe_allow_html=True)
+        st.markdown(f'<div style="font-size: 20px; text-align: center;"><div style="border: 1px solid red; padding: 10px; color: red; border-radius: 5px; display: inline-block;">{amount} {base_currency} = {converted_amount} {target_currency}</div></div>', unsafe_allow_html=True)
     else:
-        st.markdown(f'<div style="border: 1px solid red; padding: 10px; color: red; border-radius: 5px; display: inline-block;">Základní a cílová měna nemohou být stejné.', unsafe_allow_html=True)
-
+        st.markdown(f'<div style="font-size: 20px; text-align: center;"><div style="border: 1px solid red; padding: 10px; color: red; border-radius: 5px; display: inline-block;">Základní a cílová měna nemohou být stejné.', unsafe_allow_html=True)
